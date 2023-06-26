b8553098669ad8e5a6013759a60596209f8d850f
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-06-26 10:41:52 -0300

    validation complete

--- validator-fields/src/App.jsx
+++ validator-fields/src/App.jsx
@@ -1,42 +1,79 @@
-import './App.css'
+import { useState } from 'react';
+import validator from 'validator';
+import './App.css';
 
 function App() {
-  const [field, setField] = useState('')
+  const [input, setInput] = useState('');
+  const [field, setField] = useState('cpf');
+  const [valid, setValid] = useState(false);
 
-  handleValidator = () => {
+  console.log(valid);
 
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  }
+
+  const handleOption = (event) => {
+    setField(event.target.value);
+  };
+
+  const handleValidator = (event) => {
+    event.preventDefault();
+
+    switch (field) {
+      case 'hexColor':
+        setValid(validator.isHexColor(input));
+        break;
+
+      case 'isEmail':
+        setValid(validator.isEmail(input));
+        break;
+
+      case 'isURL':
+        setValid(validator.isURL(input));
+        break;
+
+      case 'cpf':
+        setValid(validator.isTaxID(input, 'pt-BR'));
+        break;
+
+      default:
+        setValid(false);
+        break;
+    }
   }
 
   return (
-    <div>
+    <div className='container'>
       <h1>
         Validador de Campos
       </h1>
-      <form>
+      <form onSubmit={handleValidator}>
         <input
           type="text"
           placeholder="Insira aqui o valor"
           id="value"
-        >
-        </input>
-        <select name="campo" id="option">
+          value={input}
+          onChange={handleInput}
+        />
+        <select name="campo" id="option" onChange={handleOption}>
           <option value='cpf'>CPF</option>
           <option value='hexColor'>Cor Hexadecimal</option>
-          <option value='email'>Email</option>
-          <option value='uuid'>UUID v4</option>
-          <option value='url'>URL</option>
+          <option value='isEmail'>Email</option>
+          <option value='isURL'>URL</option>
         </select>
-        <button
-          id="button"
-          onClick={handleValidator}
-        >
+        <button id="button" type="submit">
           Validar
         </button>
 
-        <p id="answer">Digite o valor, escolha a opção e clique em validar</p>
+        {valid ? (
+          <span>{input} é um campo válido!</span>
+        ) : (
+          <p id="answer">Digite o valor, escolha a opção e clique em validar</p>
+        )}
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
